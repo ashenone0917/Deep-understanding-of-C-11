b7637d2cbc9db63044a07bf2415d3c5a6140a189
ashenonee <fadai2333@163.com>
2021-04-15 02:39:23 +0800

    1

---
+++ template_friend/template_friend.cpp
@@ -0,0 +1,40 @@
+﻿// template_friend.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+class Poly;
+typedef Poly P;
+
+class A {
+    friend class Poly;
+};
+
+class B {
+    friend Poly;
+};
+
+class C {
+    friend P;
+};
+
+template<typename T>
+class People {
+    friend T;
+};
+int main()
+{
+    People<int> t;//对于int类型模版参数，友元声明被忽略
+    People<P> p;//这样P就是People的友元了
+    std::cout << "Hello World!\n";
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
