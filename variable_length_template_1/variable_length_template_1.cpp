d187a1405d2a00bed0255a0710d899f9901ba79f
ashenonee <caiyanchao@datacloak.com>
2021-05-09 16:00:30 +0800

    11

--- variable_length_template_1/variable_length_template_1.cpp
+++ variable_length_template_1/variable_length_template_1.cpp
@@ -2,6 +2,7 @@
 //
 
 #include <iostream>
+#include <stdexcept>
 template <typename... Elements> class mytuple;
 template <typename Head,typename... Tail>
 class mytuple<Head, Tail...> : private mytuple<Tail...> {
@@ -25,7 +26,26 @@ struct Test {
     static const long val = 10;
 };
 
-#include <future>
+void Printf(const char* s) {
+    while (*s) {
+        if (*s == '%' && *++s != '%') {
+            throw std::runtime_error("invalid format: missing arguments");
+        }
+        std::cout << *s++;
+    }
+}
+
+template<typename T,typename... Args>
+void Printf(const char* s, T value, Args... args) {
+    while (*s) {
+        if (*s == '%' && *++s != '%') {
+            std::cout << value;
+            return Printf(++s, args...);
+        }
+        std::cout << *s++;
+    }
+    throw std::runtime_error("extra arguments provided to Printf");
+}
 
 int main()
 {
@@ -33,4 +53,5 @@ int main()
     std::cout << Test::val << std::endl;
     std::cout << Multiply<2, 3, 4, 5, 6>::val << std::endl;
     std::cout << Multiply<22, 33, 44, 55, 66>::val << std::endl;
+    Printf("hello %s\n", std::string(" world"));
 }
